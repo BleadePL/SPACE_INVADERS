53554b7142b5433df206dd7dfd9b9aaf85eaca52
Konrad <konrad.wieckiewicz@gmail.com>
2021-04-20 22:52:14 +0200

    * Space ship movement functionality * Methods managment

--- space_invaders.py
+++ space_invaders.py
@@ -14,6 +14,9 @@ class SpaceInvaders:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("SPACE_INVADERS")
 
 
@@ -34,6 +37,7 @@ class SpaceInvaders:
                 #Awiting for pressing the button or pressing the mouse button
                 while True:
                     self._check_events()
+                    self.ship.update()
                     self._update_screen()
 
                 #Refreshing screen
@@ -49,12 +53,31 @@ class SpaceInvaders:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_event(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_event(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
         pygame.display.flip()
+
 if __name__ == '__main__':
     #Creating game and running it
     ai = SpaceInvaders()
